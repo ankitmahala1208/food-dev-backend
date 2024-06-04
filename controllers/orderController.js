e40c861746ff67e122acf64c412ccddf8f6e5187
chaudhari4994 <chaudharipra4994@gmail.com>
2024-06-04 11:46:42 +0530

    changes

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,13 +1,14 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
 import Stripe from 'stripe';
+import 'dotenv/config'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 //placing user order for frontend
 const placeOrder = async (req, res) => {
 
-    const frontend_url = "https://food-dev-frontend.onrender.com"; //according to frontend url
+    const frontend_url = process.env.URL; //according to frontend url
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -5,7 +5,7 @@ import { placeOrder, verifyOrder, updateStatus, userOrders, listOrders } from ".
 const orderRouter = express.Router();
 
 orderRouter.post("/place", authMiddleware, placeOrder);
-orderRouter.get("/verify", verifyOrder);
+orderRouter.post("/verify", verifyOrder);
 orderRouter.post("/userorders", authMiddleware, userOrders);
 orderRouter.get("/list", listOrders);
 orderRouter.post("/status", updateStatus);
